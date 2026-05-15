ef2757060ef6ad17fdc452b6e5b6ce7d46c24b14
amirdaadkhah <60184732+amirdaadkhah@users.noreply.github.com>
2026-05-16 00:03:26 +0200

    header desktop added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HeaderDesktopComponent } from './header-desktop/header-desktop.component';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
     AppComponent
   ],
   imports: [
-    BrowserModule, IonicModule.forRoot(), AppRoutingModule
+    BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    HeaderDesktopComponent
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
---
+++ src/app/header-desktop/header-desktop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-desktop',
+  templateUrl: './header-desktop.component.html',
+  styleUrls: ['./header-desktop.component.scss'],
+  standalone: true,
+})
+export class HeaderDesktopComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
